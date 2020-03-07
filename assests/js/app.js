233a37af50e1dee0c733fef783c06d7f5a60a676
ArtKim <null_number_13@hotmail.com>
2020-03-07 04:34:20 -0800

    adds

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,68 +1,22 @@
-// tabs is an array of titles of each site open within the window
-var Window = function(tabs) {
-    this.tabs = tabs; // we keep a record of the array inside the object
-};
+function findElement(arr, func) {
+    let num = 0;
+    arr.forEach(function(num) {
+        return num % 2 === 0;
+    });
+}
 
-// When you join two windows into one window
-Window.prototype.join = function(otherWindow) {
-    this.tabs = this.tabs.concat(otherWindow.tabs);
-    return this;
-};
+console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
 
-// When you open a new tab at the end
-Window.prototype.tabOpen = function(tab) {
-    this.tabs.push("new tab"); // let's open a new tab for now
-    return this;
-};
+// findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; }) should return 8.
 
-// When you close a tab
-Window.prototype.tabClose = function(index) {
-    // Only change code below this line
+const array = ["asdf", "frd", "gre"];
 
-    var tabsBeforeIndex = this.tabs.splice(0, index); // get the tabs before the tab
-    // var tabsAfterIndex = this.tabs.splice(index + 1); // get the tabs after the tab
+const data = array.reduce((obj, item) => {
+    if (!obj[item]) {
+        obj[item] = 0;
+    }
+    obj[item]++;
+    return obj;
+}, {});
 
-    // this.tabs = tabsBeforeIndex; // join them together
-
-    // Only change code above this line
-
-    return this;
-};
-
-// Let's create three browser windows
-var socialWindow = new Window(["FB", "Gitter", "Reddit", "Twitter", "Medium"]); // Social sites
-var videoWindow = new Window(["Netflix", "YouTube", "Vimeo", "Vine"]); //  Entertainment sites
-var workWindow = new Window(["GMail", "Inbox", "Work mail", "Docs", "freeCodeCamp"]); // Your mailbox, drive, and other work sites
-
-// Now perform the tab opening, closing, and other operations
-var finalTabs = socialWindow
-    .tabOpen() // Open a new tab for cat memes
-    .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
-    .join(workWindow.tabClose(1).tabOpen());
-console.log(finalTabs.tabs[0]);
-console.log(finalTabs.tabs[1]);
-console.log(finalTabs.tabs[2]);
-console.log(finalTabs.tabs[3]);
-console.log(finalTabs.tabs[4]);
-console.log(finalTabs.tabs[5]);
-console.log(finalTabs.tabs[6]);
-console.log(finalTabs.tabs[7]);
-console.log(finalTabs.tabs[8]);
-console.log(finalTabs.tabs[9]);
-console.log(finalTabs.tabs[10]);
-console.log(finalTabs.tabs[11]);
-
-"FB",
-    "Gitter",
-    "Reddit",
-    "Twitter",
-    "Medium",
-    "new tab",
-    "Netflix",
-    "YouTube",
-    "Vine",
-    "GMail",
-    "Work mail",
-    "Docs",
-    "freeCodeCamp",
-    "new tab";
+console.log(data);
